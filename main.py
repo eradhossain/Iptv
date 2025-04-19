ba964c12c576c4e0d8c9ec91483cd4ca9e78def6
Erad Hossain <98438334+eradhossain@users.noreply.github.com>
2025-04-19 04:49:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,3 @@ from threading import Thread
 import app
 
 Thread(target=lambda: app.app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080))), daemon=True).start()
-
-# Start Telegram bot
-bot.run_bot()
