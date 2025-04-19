bf912ff25baf7ccf6b427629c84d9699cfb929fb
Erad Hossain <98438334+eradhossain@users.noreply.github.com>
2025-04-19 04:47:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import os
 from threading import Thread
-import bot
 import app
 
-# Start Flask in background
 Thread(target=lambda: app.app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080))), daemon=True).start()
 
 # Start Telegram bot
